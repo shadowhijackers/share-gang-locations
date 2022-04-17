b0c7d5acd94d7839fb86d78d38894fbb82af8fd9
Shadow Hijackers <shadowhijackers@gmail.com>
2022-04-17 18:27:31 +0530

    Updated env

--- go.mod
+++ go.mod
@@ -6,6 +6,7 @@ require (
 	github.com/gin-gonic/gin v1.7.7
 	github.com/google/uuid v1.3.0
 	github.com/gorilla/websocket v1.5.0
+	github.com/joho/godotenv v1.4.0
 )
 
 require (
--- main.go
+++ main.go
@@ -2,19 +2,29 @@ package main
 
 import (
 	"flag"
-	"fmt"
+	"log"
+	"os"
 
+	"github.com/joho/godotenv"
 	"github.com/shadowhijackers/share-gang-locations/app"
 )
+
+func init() {
+
+	err := godotenv.Load(".env")
+
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+}
 
 func main() {
 	var port int
 	flag.IntVar(&port, "port", 8080, "Specify the port to listen to.")
 	flag.Parse()
-	addr := fmt.Sprintf(":%d", port)
-
+	// addr := fmt.Sprintf(":%d", ":" + os.Getenv("PORT"))
 	a := app.App{}
 	a.Initializer()
-	err := a.Router.Run(addr)
+	err := a.Router.Run(":" + os.Getenv("PORT"))
 	app.HandleError(err)
 }
